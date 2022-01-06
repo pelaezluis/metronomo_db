e9716ec4dbed63f1c00a589f18e48342ef794971
pelaezluis <luispelaezbedoya@gmail.com>
2022-01-06 01:03:37 -0500

    beat js funcional

--- static/js/index.js
+++ static/js/index.js
@@ -2,29 +2,30 @@
 const click1 = new Audio("/static/sound/click1.mp3");
 const click2 = new Audio("static/sound/click2.mp3");
 var isPlaying = false;
+var play = null;
+let beatsPerMeasure = 4;
+let count = 0;
 
-const getId = id => {  
-    console.log('now is ' + isPlaying)
 
-    let bpm = parseInt(id.split('-')[1])
-    let idTd = document.getElementById(id)
-
-    let click = setInterval( () => {
-        click2.play()
-    }, 60000/bpm)
-
-    if (isPlaying === false){
-        idTd.innerHTML = (
-            `<img id='${bpm}' src="https://img.icons8.com/ios-glyphs/30/000000/pause--v1.png" />`
-        );
-        console.log('tocando')
-    }
-    else if (isPlaying === true){
+const getBPM = id => {
+    let bpm = document.getElementById(id).id.split('-')[1];
+    let idTd = document.getElementById(id);
+    if(isPlaying){
+        clearInterval(play);
         idTd.innerHTML = (
             `<img id='${bpm}' src="https://img.icons8.com/ios-glyphs/30/000000/play--v1.png" />`
         );
-        console.log('pausando')
-        clearInterval(click)
+    }
+    else {
+        play = setInterval(playBeat, 60000/bpm);
+        idTd.innerHTML = (
+            `<img id='${bpm}' src="https://img.icons8.com/ios-glyphs/30/000000/pause--v1.png" />`
+        );
+        
     }
     isPlaying = !isPlaying;
+}
+
+const playBeat = () => {
+    click2.play();
 }
